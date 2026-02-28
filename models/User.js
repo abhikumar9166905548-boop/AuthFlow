25453c453d83f48f4908f5702ed53893232d54a8
abhikumar9166905548-boop <abhikumar9166905548@gmail.com>
2026-02-28 15:18:22 +0530

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type:String, unique:true },
+  mobile: String,
+  password: String,
+  otp: String,
+  otpExpiry: Date,
+  role: { type:String, default:"user" }
+},{ timestamps:true });
+
+module.exports = mongoose.model("User", userSchema);
