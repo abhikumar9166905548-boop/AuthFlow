13a260edb11c84aacaedaf49af8069d726933a2c
Abhishek <abhikumar@gmail.com>
2026-03-21 00:08:46 +0530

    Fix SMTP port 465

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,25 +2,21 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: parseInt(process.env.EMAIL_PORT) || 587,
-    secure: false, // true for 465, false for 587
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
   });
 
-  const mailOptions = {
-    from: `"Auth System" <${process.env.EMAIL_FROM}>`,
+  await transporter.sendMail({
+    from: `"Rollera" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     html,
-  };
-
-  const info = await transporter.sendMail(mailOptions);
-  console.log('📧 Email sent:', info.messageId);
-  return info;
+  });
 };
 
 module.exports = sendEmail;
